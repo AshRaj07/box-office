167e55fdb0a88406b3eb0cb7a09336ca34ea1dd2
Aishwary Rajput <aish.raj2000.a@gmail.com>
2022-07-28 12:15:34 +0530

    Added Pages and Nav

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
-import {Route,Switch} from "react-router-dom"
+import { Route, Switch } from "react-router-dom";
+import Navs from "./components/Navs";
+import Home from "./pages/Home";
+import Starred from "./pages/Starred";
 
 function App() {
   return (
-    <Switch>
-      <Route exact={true} path={"/"}>
-        This is home page
-      </Route>
-      <Route exact={true} path={"/starred"}>
-        This is starred
-      </Route>
-      <Route>
-        This is 404 page !
-      </Route>      
-    </Switch>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <Home />
+        </Route>
+        <Route exact={true} path={"/starred"}>
+          <Starred />
+        </Route>
+        <Route>This is 404 page !</Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/components/Navs.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+const Navs = () => {
+    const LINKS = [
+                  {to:"/",text:"Home Page"},
+                  {to:"/starred",text:"This is Starred Page"}  
+                ];
+  return (
+    <div>
+        <ul>
+            {LINKS.map(item=>{
+                return <li key={item.to}><Link to={item.to}>{item.text}</Link></li>
+            })}
+        </ul>
+    </div>
+  )
+}
+
+export default Navs
