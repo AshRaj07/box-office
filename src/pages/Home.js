06df404f5c77a3d814b886941f4028b6a3293c4b
Aishwary Rajput <aish.raj2000.a@gmail.com>
2022-07-28 20:13:29 +0530

    Added Api Logic and fetch the results

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,15 @@ import MainPageLayout from '../components/MainPageLayout'
 
 const Home = () => {
   const [input, setinput] = useState('');
+  const [result, setresult] = useState(null)
   const onChangeInput = (event) => {
     setinput(event.target.value);
   }
   const onClickSearch = async () => {
     try {
-      const res = await fetch(`https://api.tvmaze.com/singlesearch/shows?q=${input}`);
+      const res = await fetch(`https://api.tvmaze.com/search/shows?q=${input}`);
       const val = await res.json();
-      console.log(val);
+      setresult(val);
     } catch (error) {
       console.log(error);
     }
@@ -20,10 +21,23 @@ const Home = () => {
       onClickSearch();
     }
   }
+  const renderResult = () => {
+    if(result&&result.length===0){
+      return <div>No Results</div>
+    }
+    if(result!=null&&result.length>0){
+      console.log("fck");
+      return <div>{result.map(item=>{
+        return <div key={item.show.id}>{item.show.name}</div>
+      })}</div>
+    }
+    return null;
+  }
   return (
     <MainPageLayout>
       <input type={"text"} onChange={onChangeInput} onKeyDown={keySearch} value={input} />
       <button type={"button"} onClick={onClickSearch}>Search</button>
+      {renderResult()}
     </MainPageLayout>
   )
 }
