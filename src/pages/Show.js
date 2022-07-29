bc30055c31aee371693e6ade75ed29ce87e96836
Aishwary Rajput <aish.raj2000.a@gmail.com>
2022-07-29 23:21:09 +0530

    added error and loading logic in Show

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -4,12 +4,35 @@ import { useParams } from "react-router-dom";
 const Show = () => {
   const { id } = useParams();
   const [show, setshow] = useState(null);
+  const [loading, setloading] = useState(true)
+  const [error, seterror] = useState(null);
   useEffect(() => {
+    let isMounted = true;
     fetch(`https://api.tvmaze.com/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then((res) => res.json())
-      .then((res) => setshow(res));
+      .then((res) => {
+        if(isMounted){
+            setshow(res);
+            setloading(false)
+        }
+      })
+      .catch((err) => {
+        if(isMounted){
+            seterror(err)
+            setloading(false);
+        }
+      });
+      return () => {
+        isMounted=false;
+      }
   }, [id]);
   console.log(show);
+  if(loading){
+    return <>Data is Loading...</>
+  }
+  if(error){
+    return <>We have encountered some error</>;
+  }
   return <div>Show</div>;
 };
 
